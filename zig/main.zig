c8cc1e1b4ff8b394c83a7fb1f19ccfd9c4a42493
Diego Fernando Rojas <rojastorrado@gmail.com>
2024-06-03 15:21:54 -0500

    Simplify the config dir creation

--- zig/main.zig
+++ zig/main.zig
@@ -4,13 +4,13 @@ const fs = std.fs;
 const CONFIG_FILE_NAME = ".mobula_config";
 
 pub fn main() !void {
-    const config_dir_path = try fs.cwd().makeOpenPath(CONFIG_FILE_NAME, .{});
-    try fs.Dir.makeDir(config_dir_path, ".");
-    // if (err == fs.Error.PathAlreadyExists) {
-    //     std.debug.print("Config file already exists\n", .{});
-    //     return;
-    // }
-    // return err;
-
-    std.debug.print("Congif directory created\n", .{});
+    _ = try fs.cwd().makeOpenPath(CONFIG_FILE_NAME, .{});
+    std.debug.print("Config created successfully\n", .{});
+    //fs.Dir.makeDir(config_dir_path, ".") catch fs.Dir.makeDir(config_dir_path, ".");
+    // try fs.Dir.makeDir(config_dir_path, ".") catch |err| switch (err) {
+    //     error.PathAlreadyExists => {
+    //         std.debug.print("Directory created successfully\n", .{});
+    //     },
+    //     else => std.debug.print("Directory created successfully\n", .{}),
+    // };
 }
