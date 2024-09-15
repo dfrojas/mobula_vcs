4585d5d31c76136c7551cd9c4e18003073ba9eba
Diego Fernando Rojas <rojastorrado@gmail.com>
2024-09-15 23:31:39 +0200

    Build C with CMake

--- c/main.c
+++ c/main.c
@@ -2,17 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 
+#define CONFIG_FILE_NAME ".mobula_config"
+
 
 void init() {
+
     printf("Initializing repository...\n");
+
     FILE *fptr;
-    fptr = fopen(".mobula_config", "w");
-    fclose(fptr);
+
+    if (fptr == NULL) {
+        fptr = fopen(CONFIG_FILE_NAME, "w");
+        fclose(fptr);
+    }
+    
 }
 
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 
     char *command = argv[1];
 
